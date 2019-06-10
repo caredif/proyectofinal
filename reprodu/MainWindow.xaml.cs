aa5c9b4267f019e2e2483733ed9687a9b365e1c3
Chiva <caredifcase19@gmail.com>
2019-06-10 03:42:42 -0600

    integracion reproductor y diseño botones

--- reprodu/MainWindow.xaml.cs
+++ reprodu/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace reprodu
 {
@@ -19,10 +20,91 @@ namespace reprodu
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
+
     {
+
+        bool isDragging;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //controles del temporizador media
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+
+            isDragging = false;
+        }
+
+        void timer_Tick(object sender, EventArgs e)  //tiempo que esta reproduciendo
+        {
+            if (mePlayer.Source != null)
+            {
+                if (mePlayer.NaturalDuration.HasTimeSpan)
+                    lblStatus.Content = String.Format("{0} / {1}", mePlayer.Position.ToString(@"mm\:ss"), mePlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+
+                if (!isDragging) // Si NO hay operación de arrastre en el sliderTimeLine, su posición se actualiza cada segundo.
+                {
+                    SliderTimeLine.Value = mePlayer.Position.TotalSeconds;
+                }
+            }
+            else
+                lblStatus.Content = "No file selected...";
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            mePlayer.Play();
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            mePlayer.Pause();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            mePlayer.Stop();
+        }
+
+        private void Btnsiguiente_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SliderTimeLine_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) //no usado
+        {
+            // isDragging = false;
+            //MEmusicPlayer.Position =
+            //    TimeSpan.FromSeconds(SliderTimeLine.Value);// La posición del MediaElement se actualiza para que coincida con el progreso del sliderTimeLine.
+
+            //int sliderValue = (int)SliderTimeLine.Value;
+            //TimeSpan ts = new TimeSpan(0, 0, 0, 0, sliderValue);
+            //MEmusicPlayer.Position = ts;
+        }
+
+        private void SliderTimeLine_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            isDragging = true;
+        }
+
+        private void SliderTimeLine_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            isDragging = false;
+            mePlayer.Position =
+                TimeSpan.FromSeconds(SliderTimeLine.Value);// La posición del MediaElement se actualiza para que coincida con el progreso del sliderTimeLine.
+        }
+
+        private void SliderTimeLine_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            mePlayer.Position =
+                TimeSpan.FromSeconds(SliderTimeLine.Value);
+        }
+
+        private void BtnAtras_Click(object sender, RoutedEventArgs e)
+        {
 
         }
     }
